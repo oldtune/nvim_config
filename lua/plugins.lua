39cb305c1b96e7a733c38073fc5c8f04c3ba91a3
Do Tran <vivaladaman@gmail.com>
2024-04-05 23:46:14 +0700

    add nvim autopair

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -77,7 +77,14 @@ require("lazy").setup({
     {
         'mfussenegger/nvim-dap'
     },
-    { 'tpope/vim-commentary' }
+    { 'tpope/vim-commentary' },
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+        -- use opts = {} for passing setup options
+        -- this is equalent to setup({}) function
+    }
 })
 
 require("plugins.lsp")
