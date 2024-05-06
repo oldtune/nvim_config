37de84dfe82188084ff6f2bbded2031edb07f861
Do Tran <do.tran@saigontechnology.com>
2024-05-06 16:51:50 +0700

    add go programming language

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -18,6 +18,7 @@ return {
       null_ls.builtins.formatting.prettier,
       null_ls.builtins.formatting.rustfmt,
       null_ls.builtins.formatting.csharpier,
+      null_ls.builtins.formatting.gofmt 
     }
     return config -- return final config table
   end,
