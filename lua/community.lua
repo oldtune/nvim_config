07a021f169609edffe1f66307e6f8bc6f9bea891
Do Tran <vivaladaman@gmail.com>
2024-08-23 14:47:09 +0700

    add nvim surround

--- lua/community.lua
+++ lua/community.lua
@@ -8,6 +8,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.motion.leap-nvim" }
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
   -- import/override with your plugins folder
 }
